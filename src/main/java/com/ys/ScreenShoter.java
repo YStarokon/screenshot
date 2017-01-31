b69740b32c57f75cef3597a8b4a413cbca99dc96
YStarokon <yurikys@gmail.com>
2017-01-31 18:53:32 +0200

    Update ScreenShoter.java

--- src/main/java/com/ys/ScreenShoter.java
+++ src/main/java/com/ys/ScreenShoter.java
@@ -7,12 +7,13 @@ package com.ys;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import javax.imageio.ImageIO;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 
@@ -23,20 +24,22 @@ import ru.yandex.qatools.ashot.AShot;
  * @author yuriy
  */
 public class ScreenShoter {
+    
+    
+    private static final String BASE_DIRECTORY = "/home/yuriy/NetBeansProjects/screenShot";
 
     public static void main(String[] args) throws IOException {
         FirefoxProfile p = new FirefoxProfile();
+        FirefoxBinary binary = new FirefoxBinary(new File("/home/yuriy/work/firefox/firefox"));
+        //p.
         p.setPreference("network.proxy.no_proxies_on", "localhost, 127.0.0.1, 10.0.0.0/8, .it.loc, youtube.com, 192.168.59.0/16, .privatbank.ua, *.pb.ua");
-        WebDriver driver = new FirefoxDriver(p);
-         driver.get("http://google.com");
-        driver.findElement(By.id("lst-ib")).clear();
-        driver.findElement(By.id("lst-ib")).sendKeys("privat bank");
-        driver.findElement(By.id("lst-ib")).sendKeys(Keys.ENTER);
-        System.out.println("success");
+        WebDriver driver = new FirefoxDriver(binary, p);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.get("https://pozyczkaportal.pl");
 
         driver.close();
-
-        FileOutputStream stream = new FileOutputStream("/home/yuriy/NetBeansProjects/screenShot/img.png");
+        
+        FileOutputStream stream = new FileOutputStream(BASE_DIRECTORY+"/img.png");
         try {
             stream.write(captureScreenShot(driver));
         } finally {
@@ -54,5 +57,13 @@ public class ScreenShoter {
         byte[] imageInByte = baos.toByteArray();
         baos.close();
         return imageInByte;
+    }
+    
+    private String generateFile(){
+    
+    }
+    
+    private List<String> generateFile(){
+    
     }
 }
